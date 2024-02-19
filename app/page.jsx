684592b90613d8d8065637a480cd69d828cf74a1
Mohak Chauhan <mohak46812@gmail.com>
2024-02-20 02:06:22 +0530

    Initial Commit

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './_components/Header'
+
+const page = () => {
+  return (
+    <div>
+        <Header/>
+        Home
+    </div>
+  )
+}
+
+export default page

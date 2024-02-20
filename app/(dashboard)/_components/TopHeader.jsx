0118877fa83a54c1ff6f3a9fe6bc6c6cdd3ebf57
Mohak Chauhan <mohak46812@gmail.com>
2024-02-21 02:12:43 +0530

    Fix Header

--- app/(dashboard)/_components/TopHeader.jsx
+++ app/(dashboard)/_components/TopHeader.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const TopHeader = () => {
   return (
-    <div className='flex p-[16.65px] px-5 border-b items-center firstjustify-between md:justify-end'>
+    <div className='flex p-[16.65px] px-5 border-b items-center justify-between md:justify-end'>
         <AlignJustify className='md:hidden'/>
         <Image src='/logo.svg' width={64} height={60} className='md:hidden p-0'/>
         <UserButton afterSignOutUrl='/'/>
